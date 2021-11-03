fe2507e2332bcb6aa73b53261cc11b2619af1ab8
kayao <kayaoguzhan@windowslive.com>
2021-11-03 20:47:04 +0300

    03.11.2021 ders 1.ara

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { FormsModule } from '@angular/forms';
+
 /*Components starts*/ 
 import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
+import { VatAddedPipe } from './pipes/vat-added.pipe';
+import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 /*Components ends*/
 
 @NgModule({
@@ -16,12 +20,15 @@ import { NaviComponent } from './components/navi/navi.component';
     AppComponent,
     ProductComponent,
     CategoryComponent,
-    NaviComponent
+    NaviComponent,
+    VatAddedPipe,
+    FilterPipePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -10,6 +10,7 @@ import { ProductService } from "src/app/services/product.service";
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded: boolean = false;
+  filterText:"";
 
   constructor(private productService: ProductService,
     private activatedRoot: ActivatedRoute ) {}
